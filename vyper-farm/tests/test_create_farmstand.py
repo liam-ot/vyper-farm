bbd613878d71f9fbb1739c2815e730530dd126ec
liam <otooleli13@gmail.com>
2021-03-17 12:45:21 -0400

    @given strategies added to applicable createFarmstand tests

--- vyper-farm/tests/test_create_farmstand.py
+++ vyper-farm/tests/test_create_farmstand.py
@@ -1,5 +1,6 @@
 import brownie
 from brownie import accounts, Farm
+from brownie.test import given, strategy
 import pytest
 
 # contract deployment fixture #
@@ -24,23 +25,21 @@ def test_stand_created_owner_updates(deploy):
 
     assert owner == accounts[0]
 
-def test_stand_name_set(deploy):
-    name = 'My Farm Stand'
-
-    deploy.createFarmStand(name, '123 Valid Location State, 00000', accounts[0])
+@given(test_name=strategy('string', max_size=256))
+def test_stand_name_set(deploy, test_name):
+    deploy.createFarmStand(test_name, '123 Valid Location State, 00000', accounts[0])
     
     standName = deploy.getStandName()
 
-    assert standName == name
+    assert standName == test_name
 
-def test_stand_location_set(deploy):
-    location = '123 Valid Location State, 00000'
-
-    deploy.createFarmStand('My Farm Stand', location, accounts[0])
+@given(test_location=strategy('string', max_size=1024))
+def test_stand_location_set(deploy, test_location):
+    deploy.createFarmStand('My Farm Stand', test_location, accounts[0])
     
     standLocation = deploy.getStandLocation()
 
-    assert standLocation == location
+    assert standLocation == test_location
 
 def test_stand_wallet_set(deploy):
     wallet = accounts[0]
@@ -53,17 +52,19 @@ def test_stand_wallet_set(deploy):
 
 # unhappy results #
 
-def test_stand_created_location_length(deploy):
+@given(test_location_fail=strategy('string', min_size=513, max_size=514))
+def test_stand_created_location_length(deploy, test_location_fail):
     with brownie.reverts():
-        deploy.createFarmStand('My Farm Stand', 'ax2aBrlbijRUBCGSCKsMmY3nmLHTKeL6fakJ94huAS9s7nsFjEmMUAg8nAYGldygkhdG4n0CUi0eyXzbpCaOnsrCLWR0nYl5DQecy2hNb9LImZd6miPEh8kdVSyAOkV2yd7xe8owMo6VCro7DdLdqLdPHcuJd6t0pLvkLBdSm8uMeROkXyuSlfz1fkF5fSZskfPARXDoQPelWeIJtvmK6SDMMkptGVV6J0EFnWeBhT46L537L1K5IjsbS0BaWxg1wL1wdQgDEAdnf334IgIlxjGNpJACPe8dhMY8IegrNGZy6QMAH2DmMPvPrxCkptdpCsT7heMCKYzETBambGZTR07xmlO102oQueF3CfBNg0mYIJ3BE192kdzjlLqC4KzaRGVLcaP2ltiG1sLr4mBcXO5aVferEqOO4W36ts1cl3kdEyHL5KR1fYsUXcOGkZXaj4086ZblaX1vgbM0p7sgQ72XXvSjBmOvgSnoUTCvIhZBUm8ibj8fkqaJYND3fDnS7RKhjqnEdUFytExZiLbDob6kd4gx3NbHsuCONqSfWDMfrbB88ovcsL71gOHOTNSIYuVBmOKWm7P6cwMhLMhThdQt6gSZWU7pqw9yvOoT6Ox9rc8AEftUTbBmSzkNUSsaC7l48dyRiGn9JkSsd13IPFJH17IgkzgL7eBC7GgwQT3eeGzTDQy92fWfj93VvEko3k9UchUwlbGMrZ8cMhi0Ra40dHjsw7yUCtF23obMfEDSAIf8CyT8PZ21PB9iWDtu8eVFPhG30LiSTtvPbCLhB736BiXmZi5pbF3EUDD6kM4etGhLvmHluFXrargMgcxHeYpIVNIMw3x0ERV78ci3fIFpmhkJBwAaDlQkTDTopuMyhm7pMyvKyk6DXmiHw9lxwkYZh0wWGPMq8TrKj7irgSxIvPlNTAYXxZHEy667WzTT3bL6dE1E1kSidHLGlpk46TS8EI1XGJHpRUtkFml7nHdW57M34WAyJQVbHEDUaLpMH9CZnw2A75JyivVCnPzey', accounts[0])
+        deploy.createFarmStand('My Farm Stand', test_location_fail, accounts[0])
 
 def test_stand_created_owner_verification(deploy):
     with brownie.reverts():
         deploy.createFarmStand('My Farm Stand', '123 Valid Location State, 00000', accounts[9])
 
-def test_stand_created_name_length(deploy):
+@given(test_name_fail=strategy('string', min_size=257, max_size=258))
+def test_stand_created_name_length(deploy, test_name_fail):
     with brownie.reverts():
-        deploy.createFarmStand('CEcHHttPZTeKTGkRnNjevfi8GNYU8qNvjVHwSQhxgMrfWjLWQfWRbkhtwMESiHTWZVG2GDk49jx6khbqzTnrpGxcNRSG3k3rTeEQZ2SMycj6bBx4ZdW92US3FPwAPCrLXaXRGEfd7iUCaxBiVQvzDgLLvZA2dnb8RdvgC8tXB8rWc5AWQnKywGYfG7WAkWVztcJTbhPChkBE9v2BiSvcD5c8P9m6Nb6VddX9CNDrDpEpKtqeZPxv9DZuve8awyiNu', '123 Valid Location State, 00000', accounts[0])
+        deploy.createFarmStand(test_name_fail, '123 Valid Location State, 00000', accounts[0])
 
 def test_stand_created_status_verification(deploy):
     deploy.createFarmStand('My Farm Stand', '123 Valid Location State, 00000', accounts[0])
